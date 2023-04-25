f8b18991d0ae202e35b201c2e303971487f8dbc5
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-25 22:24:46 +0200

    Spring Boot - Day 1

---
+++ src/main/java/com/tpe/service/StudentService.java
@@ -0,0 +1,20 @@
+package com.tpe.service;
+
+import com.tpe.domain.Student;
+import com.tpe.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+
+    public List<Student> getAll() {
+        return studentRepository.findAll();
+    }
+}
