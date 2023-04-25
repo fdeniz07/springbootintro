8031e17a36be5e58ccfa8d7e6158557682d0678f
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-25 22:24:38 +0200

    Spring Boot - Day 1

---
+++ src/main/java/com/tpe/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.tpe.repository;
+
+import com.tpe.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository //Opsiyonel
+public interface StudentRepository extends JpaRepository<Student,Long> {
+
+
+
+}
