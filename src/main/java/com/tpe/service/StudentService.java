83ff87e76829f4ffd19a7b24d9246a7d0885972f
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-26 22:28:18 +0200

    Spring Boot - Day 2

--- src/main/java/com/tpe/service/StudentService.java
+++ src/main/java/com/tpe/service/StudentService.java
@@ -1,6 +1,8 @@
 package com.tpe.service;
 
 import com.tpe.domain.Student;
+import com.tpe.exception.ConflictException;
+import com.tpe.exception.ResourceNotFoundException;
 import com.tpe.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +18,24 @@ public class StudentService {
 
     public List<Student> getAll() {
         return studentRepository.findAll();
+    }
+
+
+    public void createStudent(Student student) {
+
+        if (studentRepository.existsByEmail(student.getEmail())) {
+            throw new ConflictException("Email is already exist!");
+        }
+
+        studentRepository.save(student);
+    }
+
+    public Student findStudent(Long id) {
+        return studentRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Student not found with id : " + id));
+    }
+
+    public void deleteStudent(Long id) {
+        Student student = findStudent(id);
+        studentRepository.delete(student);
     }
 }
