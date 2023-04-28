57109668af42390a5924bc1cdfd32987f1d56b1e
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-28 17:38:16 +0200

    Spring Boot - Day 3

--- src/main/java/com/tpe/repository/StudentRepository.java
+++ src/main/java/com/tpe/repository/StudentRepository.java
@@ -4,8 +4,12 @@ import com.tpe.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository //Opsiyonel
 public interface StudentRepository extends JpaRepository<Student,Long> {
 
     boolean existsByEmail(String email);
+
+    List<Student> findByLastName(String lastName);
 }
