bb548a7e47bb76198296cfe6784a2a5a7da483c7
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-28 21:03:34 +0200

    Spring Boot - Day 4

--- src/main/java/com/tpe/repository/StudentRepository.java
+++ src/main/java/com/tpe/repository/StudentRepository.java
@@ -1,15 +1,32 @@
 package com.tpe.repository;
 
 import com.tpe.domain.Student;
+import com.tpe.dto.StudentDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository //Opsiyonel
-public interface StudentRepository extends JpaRepository<Student,Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
     boolean existsByEmail(String email);
 
     List<Student> findByLastName(String lastName);
+
+
+    //JPQL
+    @Query("SELECT s FROM Student s WHERE s.grade=:pGrade")
+    List<Student> findAllEqualsGrade(@Param("pGrade") Integer grade);
+
+
+    //SQL
+    @Query(value= "SELECT * FROM Student s WHERE s.grage=:pGrade",nativeQuery = true )
+    List<Student> findAllEqualsGradeWithSQL(@Param("pGrade") Integer grade);
+
+    @Query("SELECT new com.tpe.dto.StudentDto(s)  FROM Student s WHERE s.id:id")
+    Optional<StudentDto> findStudentDTOById(Long id);
 }
